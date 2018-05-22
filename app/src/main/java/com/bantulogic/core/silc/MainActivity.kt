bf4ecba2786cb9f1e2694132aae77e433e0b7f7c
Chaiwa Berian <chaiwa.berian@gmail.com>
2018-05-22 20:57:45 +0200

    Add Menu Placeholder

--- app/src/main/java/com/bantulogic/core/silc/MainActivity.kt
+++ app/src/main/java/com/bantulogic/core/silc/MainActivity.kt
@@ -2,11 +2,19 @@ package com.bantulogic.core.silc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolBar))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
